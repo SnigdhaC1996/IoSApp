f1e557b92105ff7f376c89f8e50013fb3bbf4584
SnidhaC1996 <scjuly96@gmail.com>
2023-08-20 15:08:07 +0530

    Initial Commit

---
+++ EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Snigdha Chaudhari on 20/08/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
